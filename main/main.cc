c6a92f5b4195f1097c8dfa9cb25d3052071942e2
Dawid Wysocki <tortillazhawaii@gmail.com>
2023-10-22 14:28:25 +0200

    Add benchmark example

--- main/main.cc
+++ main/main.cc
@@ -1,6 +1,16 @@
+#include "../utils/benchmark/benchmark.h"
+#include <chrono>
 #include <iostream>
+#include <thread>
 
 int main() {
   std::cout << "Hello, world!" << std::endl;
+  auto benchmark = rr::utils::Benchmark(
+      "No data", "Just wait",
+      []() { std::this_thread::sleep_for(std::chrono::seconds(1)); }, 3);
+
+  auto result = benchmark.run();
+
+  std::cout << result.to_string() << std::endl;
   return 0;
 }
