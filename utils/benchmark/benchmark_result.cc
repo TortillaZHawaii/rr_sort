881a360092988766d54d827fcd303ac93e374535
Dawid Wysocki <tortillazhawaii@gmail.com>
2023-10-22 21:20:04 +0200

    Change to_string order

--- utils/benchmark/benchmark_result.cc
+++ utils/benchmark/benchmark_result.cc
@@ -12,11 +12,12 @@ std::string BenchmarkResult::to_string() const {
       "Is correct: " +
       std::string(number_of_correct_runs == number_of_runs ? "yes" : "no") +
       "\n";
+  result += "Min time: " + std::to_string(min_time_ns) + " ns\n";
+
   result += "Max time: " + std::to_string(max_time_ns) + " ns\n";
-  result += "Average time: " + std::to_string(average_time_ns) + " ns\n";
   result +=
       "Standard deviation: " + std::to_string(standard_deviation_ns) + " ns\n";
-  result += "Min time: " + std::to_string(min_time_ns) + " ns\n";
+  result += "Average time: " + std::to_string(average_time_ns) + " ns\n";
   return result;
 }
 
