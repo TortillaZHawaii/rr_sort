5fec653335133152db9c79ab9dee892d65ab143e
Damian Górski <d.gorski.1@stud.elka.pw.edu.pl>
2024-01-23 02:00:19 +0100

    additional ` characters v2

--- parallel_with_shared_memory/Spark_Sort/src/main/java/com/example/TimSortInMapReduceStyle.java
+++ parallel_with_shared_memory/Spark_Sort/src/main/java/com/example/TimSortInMapReduceStyle.java
@@ -1,4 +1,4 @@
-package com.example;
+ package com.example;
 
 import org.apache.spark.SparkConf;
 import org.apache.spark.api.java.JavaRDD;
@@ -8,6 +8,8 @@ import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.PriorityQueue;
+
 
 public class TimSortInMapReduceStyle implements MySort, Serializable {
 
@@ -18,11 +20,11 @@ public class TimSortInMapReduceStyle implements MySort, Serializable {
         SparkConf conf = new SparkConf().setAppName("tim-sort-in-map-reduce-style");
         JavaSparkContext sc = new JavaSparkContext(conf);
 
-        JavaRDD<String> lines = sc.textFile(inputPath);
+        JavaRDD<String> lines = sc.textFile(inputPath).repartition(2);
         JavaRDD<String> words = lines.flatMap(line -> Arrays.asList(line.split(" ")).iterator());
 
         JavaRDD<String> sortedWords = words.mapPartitions(iterator -> {
-            List<String> list = new ArrayList<>();
+            List<String> list = new java.util.ArrayList<>();
             while (iterator.hasNext()) {
                 list.add(iterator.next());
             }
@@ -32,13 +34,51 @@ public class TimSortInMapReduceStyle implements MySort, Serializable {
             return list.iterator();
         });
 
-        // Opcjonalne sortowanie ostateczne
-        // sortedWords = sortedWords.sortBy(word -> word, true, 1);
+        JavaRDD<String> mergedList =sortedWords.coalesce(1);
+        JavaRDD<String> sortedMergedWords = mergedList.mapPartitions(iterator -> {
+            List<String> list = new java.util.ArrayList<>();
+            while (iterator.hasNext()) {
+                list.add(iterator.next());
+            }
 
-        sortedWords.saveAsTextFile(outputPath);
+            List<String> mergedKSotedList = mergeKSortedLists(list, 40);
+
+            return mergedKSotedList.iterator();
+        });
+
+        sortedMergedWords.saveAsTextFile(outputPath);
 
         sc.stop();
         sc.close();
+    }
+
+    public List<String> mergeKSortedLists(List<String> input, int k) {
+        if (input == null || input.isEmpty()) {
+            // return new ArrayList<>();
+            throw new RuntimeException("Input list is empty");
+        }
+
+        PriorityQueue<String> minHeap = new PriorityQueue<>();
+        List<String> result = new ArrayList<>();
+
+        int groups = 0;
+        for (String element : input) {
+            minHeap.offer(element);
+            groups++;
+
+            if (groups == k) {
+                while (!minHeap.isEmpty()) {
+                    result.add(minHeap.poll());
+                }
+                groups = 0;
+            }
+        }
+
+        while (!minHeap.isEmpty()) {
+            result.add(minHeap.poll());
+        }
+
+        return result;
     }
 
     private void timSort(List<String> list) {
@@ -52,10 +92,10 @@ public class TimSortInMapReduceStyle implements MySort, Serializable {
             insertionSort(sublist);
         }
 
-        mergeSortedChunks(list, chunkSize);
+        insertionSort(list);
     }
 
-    private void insertionSort(List<String> list) {
+    private static void insertionSort(List<String> list) {
         int n = list.size();
         for (int i = 1; i < n; ++i) {
             String key = list.get(i);
@@ -66,43 +106,6 @@ public class TimSortInMapReduceStyle implements MySort, Serializable {
                 j = j - 1;
             }
             list.set(j + 1, key);
-        }
-    }
-
-    private void mergeSortedChunks(List<String> list, int chunkSize) {
-        int n = list.size();
-
-        for (int size = chunkSize; size < n; size *= 2) {
-            for (int left = 0; left < n; left += 2 * size) {
-                int mid = Math.min(left + size, n);
-                int right = Math.min(left + 2 * size, n);
-
-                List<String> leftList = list.subList(left, mid);
-                List<String> rightList = list.subList(mid, right);
-
-                List<String> merged = new ArrayList<>(leftList.size() + rightList.size());
-                int i = 0, j = 0;
-
-                while (i < leftList.size() && j < rightList.size()) {
-                    if (leftList.get(i).compareTo(rightList.get(j)) <= 0) {
-                        merged.add(leftList.get(i++));
-                    } else {
-                        merged.add(rightList.get(j++));
-                    }
-                }
-
-                while (i < leftList.size()) {
-                    merged.add(leftList.get(i++));
-                }
-
-                while (j < rightList.size()) {
-                    merged.add(rightList.get(j++));
-                }
-
-                for (int k = 0; k < merged.size(); k++) {
-                    list.set(left + k, merged.get(k));
-                }
-            }
         }
     }
 }
